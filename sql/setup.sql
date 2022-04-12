e08a9d9aff0a3f436cd7ad2bd9cbb0bee5d3b787
Zachary Caldwell <85139250+zcaldwell@users.noreply.github.com>
2022-04-12 15:41:24 -0700

    Zach dev (#2)
    * user signout test written and passing
    
    * create fruit tree resource tested and passing

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS fruit_trees CASCADE;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -9,3 +10,12 @@ CREATE TABLE users (
     password_hash TEXT NOT NULL
 );
 
+CREATE TABLE fruit_trees (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT REFERENCES users(id),
+    latitude DEC(8, 6),
+    longitude DEC(9, 6),
+    type TEXT NOT NULL,
+    description TEXT NOT NULL,
+    permission BOOLEAN NOT NULL
+);
