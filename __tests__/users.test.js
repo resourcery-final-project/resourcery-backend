3d33430b8199daeaf93d40220a65de38f5fc19c1
Bailey Kuebelbeck <71611929+baileykue@users.noreply.github.com>
2022-04-12 14:17:42 -0700

    Bailey dev (#1)
    * wrote first test, controller, model, and user service for signing up a user
    
    * wrote test, controller, model, and user service for signin in a user
    
    * adding secrets to yml file

---
+++ __tests__/users.test.js
@@ -0,0 +1,42 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+describe('resourcery-backend routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('should sign up a user', async () => {
+    const newUser = {
+      username: 'cake',
+      password: '12345',
+    };
+
+    const res = await request(app).post('/api/v1/users').send(newUser);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'cake',
+    });
+  });
+
+  it('should sign in a user', async () => {
+    const userObj = {
+      username: 'rose',
+      password: 'petal',
+    };
+
+    const user = await UserService.insert(userObj);
+
+    const res = await request(app).post('/api/v1/users/session').send(userObj);
+
+    expect(res.body).toEqual({ message: 'you are signed in!', user });
+  });
+});
